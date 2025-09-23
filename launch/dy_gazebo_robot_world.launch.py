62dbb62f734a90aff2ce5aa6df5ebe30309628f0
sunwei-ksdy-robot <sunweijm.sun@gmail.com>
2025-09-23 16:01:51 +0800

    add a world only launch file

---
+++ launch/dy_gazebo_robot_world.launch.py
@@ -0,0 +1,76 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    robot_name_in_model = 'dy_robot'
+    package_name = 'robot_description'
+    # xacro_name = "sentry_robot_sim.xacro"
+
+    pkg_share = FindPackageShare(package=package_name).find(package_name) 
+    # xacro_model_path = os.path.join(pkg_share, f'urdf/{xacro_name}')
+    gazebo_world_path = os.path.join(pkg_share, 'world/carto_test.world') #mini_world.world
+
+    # Start Gazebo server
+    start_gazebo_cmd =  ExecuteProcess(
+        cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', gazebo_world_path],
+        output='screen')
+
+    # doc = xacro.parse(open(xacro_model_path))
+    # xacro.process_doc(doc)
+    # params = {'robot_description': doc.toxml()}
+
+    # node_robot_state_publisher = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[params]
+    # )
+
+    # Launch the robot
+    # spawn_entity_cmd = Node(
+    #     package='gazebo_ros', 
+    #     executable='spawn_entity.py',
+    #     arguments=['-entity', robot_name_in_model,  '-topic', params], 
+    #     output='screen')
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_diff_drive_base_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_drive_base_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=spawn_entity_cmd,
+        #         on_exit=[load_joint_state_broadcaster],
+        #     )
+        # ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_diff_drive_base_controller],
+            )
+        ),
+
+        start_gazebo_cmd,
+        # node_robot_state_publisher,
+        # spawn_entity_cmd,
+    ])
